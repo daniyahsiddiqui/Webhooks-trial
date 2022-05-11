878e49d6a3e46448850c8e0d95021b42e27e851d
Daniyah <101661796+daniyahsiddiqui@users.noreply.github.com>
2022-05-11 01:11:25 -0500

    Update iac_usecase_2.tf

--- Labs/usecases/usecase-2/terraform/green/iac_usecase_2.tf
+++ Labs/usecases/usecase-2/terraform/green/iac_usecase_2.tf
@@ -58,9 +58,6 @@ resource "aws_security_group" "basic_http" {
   name = "sg_flask-usecase2-green"
   description = "Web Security Group for HTTP"
   vpc_id =  var.VPC
-  lifecycle {
-    create_before_destroy = true
-  }
   ingress = [
     {
       description = "Allow HTTP Traffic access"
